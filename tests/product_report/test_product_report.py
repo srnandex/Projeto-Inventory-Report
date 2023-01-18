930db12a3d2eee61f8868150fac947d57dd0c363
Fernando Rocha <fernandodasilvarocha2@gmail.com>
2023-01-18 05:11:16 -0300

    requisito 8

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,22 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "CADEIRA",
+        "Forces of Nature",
+        "2022-04-04",
+        "2023-02-09",
+        "FR48",
+        "Conservar em local fresco",
+    )
+
+    assert product.__repr__() == (
+        f"O produto {product.nome_do_produto} "
+        f"fabricado em {product.data_de_fabricacao} "
+        f"por {product.nome_da_empresa} com validade "
+        f"até {product.data_de_validade} "
+        f"precisa ser armazenado {product.instrucoes_de_armazenamento}."
+    )
+    assert type(product.__repr__())
