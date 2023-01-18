71cd7cf942baeb8e2dd9cfb5f4c1048cc9da2d2e
Fernando Rocha <fernandodasilvarocha2@gmail.com>
2023-01-18 00:48:16 -0300

    requisito 2

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,24 @@
+from datetime import datetime
+import statistics
+
+
 class SimpleReport:
-    pass
+    @classmethod
+    def generate(self, list):
+        fabric = min(
+            [product["data_de_fabricacao"] for product in list]
+        )
+
+        validade = min(
+            [product["data_de_validade"] for product in list
+                if product["data_de_validade"] > str(datetime.today())]
+        )
+
+        companys = [company['nome_da_empresa'] for company in list]
+        mais_produtos = statistics.mode(companys)
+
+        return (
+          f"Data de fabricação mais antiga: {fabric}\n"
+          f"Data de validade mais próxima: {validade}\n"
+          f"Empresa com mais produtos: {mais_produtos}"
+        )
