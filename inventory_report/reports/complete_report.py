80757a6ab02da791e9f55b29eacd8140f8046704
Fernando Rocha <fernandodasilvarocha2@gmail.com>
2023-01-18 03:48:50 -0300

    requisito 3

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,19 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
+
+
+class CompleteReport(SimpleReport):
+    @classmethod
+    def generate(self, list):
+        simple = SimpleReport.generate(list)
+        products = Counter(item['nome_da_empresa'] for item in list)
+        stored = ''
+
+        for company in products:
+            stored += f'- {company}: {products[company]}\n'
+
+        return (
+          f"{simple}\n"
+          f"Produtos estocados por empresa:\n"
+          f"{stored}"
+        )
